27a738927b3b6e7c0cf38e5e84a743bf343b2412
Ivashin Ruslan <ruslanmfj@gmail.com>
2022-02-17 17:01:04 +0300

    Добавьте файлы проекта.

---
+++ NmarketTestTask/Parsers/ExcelParser.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ClosedXML.Excel;
+using NmarketTestTask.Models;
+
+namespace NmarketTestTask.Parsers
+{
+    public class ExcelParser : IParser
+    {
+        public IList<House> GetHouses(string path)
+        {
+            var workbook = new XLWorkbook(path);
+            var sheet = workbook.Worksheets.First();
+            var houses = sheet.Cells().Where(c => c.GetValue<string>().Contains("Дом")).ToList();
+            List<House> housesList = new List<House>(); 
+            //Реализация цикла, внутри которого идёт поиск квартир в рамках одного дома
+            for (int houseID = 0; houseID != houses.Count(); houseID++)
+            {
+                List<Flat> _flats = new List<Flat>();
+                //Цикл поиска квартир от строки в которой находиться дом до следующего дома(либо вниз на 25 строк)
+                for (int RowID = houses[houseID].WorksheetRow().RowNumber()+1; RowID != houses[houses.Count()-1].WorksheetRow().RowNumber()+25; RowID++)
+                {
+                    for (int ColumnID = 1; ColumnID < 16383; ColumnID++)
+                    {
+                        if (sheet.Cell(RowID, ColumnID).Value.ToString() != "")
+                            if (sheet.Cell(RowID, ColumnID).Value.ToString().Contains("Дом")) break;
+                        //Реализация справделива, только в случае, если под ячейкой квартиры находиться сумма и квартиры пронумерованы
+                            else if (sheet.Cell(RowID, ColumnID).Value.ToString().Contains('№'))
+                            {
+                                var _flatcell = sheet.Cell(RowID, ColumnID);
+                                _flats.Add(new Flat() { Number = _flatcell.Value.ToString(), Price = _flatcell.CellBelow().Value.ToString() });
+                            }
+                    }
+                }
+                housesList.Add(new House()
+                {
+                    Name = houses[houseID].Value.ToString(),
+                    Flats = _flats
+                });
+            }
+            return housesList;
+        }
+    }
+}
---
+++ NmarketTestTask/Parsers/HtmlParser.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+
+using System.Collections.Generic;
+using HtmlAgilityPack;
+using NmarketTestTask.Models;
+
+namespace NmarketTestTask.Parsers
+{
+    public class HtmlParser : IParser
+    {
+        public IList<House> GetHouses(string path)
+        {
+            List<House> houses = new List<House>();
+
+            var doc = new HtmlDocument();
+            doc.Load(path);
+            var nodes = doc.DocumentNode.SelectNodes(".//td");
+            foreach (var node in nodes)
+            {
+                var nodeClases = node.GetClasses();
+                foreach (var nodeClass in nodeClases)
+                {
+                    if (nodeClass.ToString() == "house")
+                    {
+                        if (HouseDublicator(doc, node.InnerText, houses) == false)
+                        {
+                            houses.Add(new House()
+                            {
+                                Name = node.InnerText,
+                                Flats = flatsOfHouse(doc, node.InnerText)
+                            });
+                        }
+                    }
+                }
+            }
+            return houses;
+        }
+
+
+        /// <summary>
+        /// Поиск квартир по заданному номеру дома
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="house"></param>
+        /// <returns></returns>
+        public List<Flat> flatsOfHouse(HtmlDocument doc, string house)
+        {
+            var flats = new List<Flat>();
+            var nodes = doc.DocumentNode.SelectNodes(".//td");
+            foreach (var node in nodes)
+            {
+                var nodeClasses = node.GetClasses();
+                foreach (var nodeClass in nodeClasses)
+                {
+                    if (nodeClass.ToString() == "house")
+                        if (node.InnerText == house)
+                        {
+                            var flatNumber = nodes[nodes.IndexOf(node)+1];
+                            var flatPrice = nodes[nodes.IndexOf(node)+2];
+                            flats.Add(new Flat() { Number = flatNumber.InnerText, Price = flatPrice.InnerText });
+                        }
+                }
+            }
+            return flats;
+        }
+
+
+        /// <summary>
+        /// Исключает повторное добавление дома в объект
+        /// </summary>
+        /// <param name="doc"></param>
+        /// <param name="houseName"></param>
+        /// <param name="houses"></param>
+        /// <returns></returns>
+        public bool HouseDublicator(HtmlDocument doc, string houseName, List<House> houses)
+        {
+            bool result = false;    
+            foreach (var house in houses)
+            {
+                if (house.Name == houseName) result = true;
+            }
+            return result;  
+        }
+    }
+}
